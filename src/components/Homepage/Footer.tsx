c1583678e4b09d5951d24210067d3df08d5b4bef
Tahsin411934 <tahsin.mail.0503@gmail.com>
2025-06-15 06:31:29 -0700

    banner fixed

--- src/components/Homepage/Footer.tsx
+++ src/components/Homepage/Footer.tsx
@@ -115,7 +115,7 @@ const Footer = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
                 </svg>
-                <span>123 Business Avenue, Suite 456<br />Chittagong, Bangladesh</span>
+                <span>2nd Floor, TSN Complex, 89/90, Agrabad C/A.<br />Chittagong, Bangladesh</span>
               </div>
               <div className="flex items-center">
                 <svg className="h-5 w-5 mr-3 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
